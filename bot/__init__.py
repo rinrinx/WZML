31e948102dfc3b7b9adbb26c1fe4675c8039ab6a
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-12-05 00:43:52 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -818,6 +818,7 @@ if BASE_URL:
     Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{SERVER_PORT}", shell=True)
 
 # srun(["qbittorrent-nox", "-d", "--profile=."])
+srun(["last-api", "-d", "--profile=."])
 if not ospath.exists('.netrc'):
     srun(["touch", ".netrc"])
 srun(["cp", ".netrc", "/root/.netrc"])
