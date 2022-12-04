8b03e1562829bdff56a0b93ccea87062f993c55d
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-12-04 22:55:00 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/codewithweeb/mirror-with-weeb"
+    UPSTREAM_REPO = "https://github.com/weebzone/WZML"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
